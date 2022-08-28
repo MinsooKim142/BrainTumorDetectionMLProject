78678ea057aea5e209d10d7f1a8360abc1a9f423
Msk142 <khjun7830@gmail.com>
2022-08-28 18:21:29 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+
+#Import dataset
+
+
+
+
+#Build linear model
+
+
+#Create a model
+
+
+#Use Model
+
+
+
+#Train Model
