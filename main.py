091344e317524c074043d0351f906bab23caf63c
Msk142 <khjun7830@gmail.com>
2022-08-29 02:29:23 -0400

    snd

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
+from tensorflow import keras
+import PIL
+import PIL.Image
+import numpy as np
 
 
+print(tf.__version__)
 #Import dataset
+
+trainDataset = train.flow_from_directory()
 
 
 
