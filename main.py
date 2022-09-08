950ec6a415d2b1a427127078e61a8e40cf4f1a7f
Msk142 <khjun7830@gmail.com>
2022-09-08 17:02:37 -0400

    commit 123

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ import numpy as np
 print(tf.__version__)
 #Import dataset
 
-trainDataset = train.flow_from_directory()
+#FIXME Import dataset from directory
+trainDataset = train.flow_from_directory("https://github.com/MinsooKim142/BrainTumorDetectionMLProject/blob/f2bc35d418d9f3cab2a3153aa182d623fe0f146b/dataset/no")
+
+
 
 
 
