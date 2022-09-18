4f8dbd360cfa629701535033847fe782e060dc8d
Msk142 <khjun7830@gmail.com>
2022-09-18 02:39:10 -0400

    large update

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from tensorflow import keras
 import PIL
 import PIL.Image
 import numpy as np
+import pathlib
 
 
 print(tf.__version__)
 #Import dataset
+
+dataset_url = "https://github.com/MinsooKim142/BrainTumorDetectionMLProject/blob/f2bc35d418d9f3cab2a3153aa182d623fe0f146b/dataset/no"
 
 #FIXME Import dataset from directory
 trainDataset = train.flow_from_directory("https://github.com/MinsooKim142/BrainTumorDetectionMLProject/blob/f2bc35d418d9f3cab2a3153aa182d623fe0f146b/dataset/no")
